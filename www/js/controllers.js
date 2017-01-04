3275ea8367285c0a1130d927805b2ac9edccaa95
Soham Banerjee <soham2008xyz@gmail.com>
2017-01-04 18:38:56 +0530

    Address detection added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,10 +7,6 @@ angular.module('map.controllers', [])
 .controller('HomeController', HomeController);
 
 function HomeController($scope, $cordovaGeolocation) {
-  function initialize() {
-
-    $scope.map = map;
-  }
 
   $scope.locate = function () {
     var posOptions = {
@@ -24,23 +20,41 @@ function HomeController($scope, $cordovaGeolocation) {
         var long = position.coords.longitude;
 
         //$scope.$apply(function () {
-          console.log(lat, long);
-          var myLatlng = new google.maps.LatLng(lat, long);
-          var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-          };
-          var map = new google.maps.Map(
-            document.getElementById("map"),
-            mapOptions
-          );
-          var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            title: 'My Location'
-          });
-
+        console.log(lat, long);
+        var myLatlng = new google.maps.LatLng(lat, long);
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(
+          document.getElementById("map"),
+          mapOptions
+        );
+        var geocoder = new google.maps.Geocoder;
+        var infowindow = new google.maps.InfoWindow;
+        var latlng = {
+          lat: lat,
+          lng: long
+        };
+        geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[1]) {
+              //map.setZoom(11);
+              var marker = new google.maps.Marker({
+                position: latlng,
+                map: map
+              });
+              map.setCenter(latlng);
+              infowindow.setContent(results[1].formatted_address);
+              infowindow.open(map, marker);
+            } else {
+              console.log('No results found');
+            }
+          } else {
+            console.log('Geocoder failed due to: ' + status);
+          }
+        });
         //});
       }, function(err) {
         // error
